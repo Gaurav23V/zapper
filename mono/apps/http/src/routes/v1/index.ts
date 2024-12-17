54f18fa6c7576025c0dd7c90ee02c0191389277e
MajorVick <126904019+MajorVick@users.noreply.github.com>
2024-12-17 18:04:06 +0000

    updated some parts of the schema and wrote the signup endpoint

--- mono/apps/http/src/routes/v1/index.ts
+++ mono/apps/http/src/routes/v1/index.ts
@@ -2,11 +2,35 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
+import { SignupSchema } from "../../types";
+import client from "@repo/db/client";
 
 export const router = Router();
 
-router.post("/signup", (req, res) => {
-  res.json({ message: "Signup!" });
+router.post("/signup", async (req, res) => {
+  // check the user
+  const parsedData = SignupSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({ error: parsedData.error });
+    return;
+  }
+
+  try {
+    // create the user
+    const user = await client.user.create({
+      data: {
+        username: parsedData.data.username,
+        password: parsedData.data.password,
+        role: parsedData.data.type === "admin" ? "Admin" : "User",
+      },
+    });
+    res.json({
+      userId: user.id,
+    });
+  } catch (e) {
+    res.status(400).json({ error: "User already exists" });
+    return;
+  }
 });
 
 router.post("/signin", (req, res) => {
---
+++ mono/apps/http/src/types/index.ts
@@ -0,0 +1,58 @@
+import z from "zod";
+
+export const SignupSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(8),
+  type: z.enum(["user", "admin"]),
+});
+
+export const SigninSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(8),
+});
+
+export const updateMetadataSchema = z.object({
+  avatarId: z.string(),
+});
+
+export const createSpaceSchema = z.object({
+  name: z.string(),
+  // custom function that validates 100x100 schema
+  dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+  mapId: z.string(),
+});
+
+export const addElementSchema = z.object({
+  spaceId: z.string(),
+  elementId: z.string(),
+  x: z.number(),
+  y: z.number(),
+});
+
+export const createElementSchema = z.object({
+  imageUrl: z.string(),
+  width: z.number(),
+  height: z.number(),
+  static: z.boolean(),
+});
+
+export const UpdateElementSchema = z.object({
+  imageUrl: z.string(),
+});
+
+export const createAvatarSchema = z.object({
+  imageUrl: z.string(),
+  name: z.string(),
+});
+
+export const createMapSchema = z.object({
+  thumbnail: z.string(),
+  dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+  defaultElements: z.array(
+    z.object({
+      elementId: z.string(),
+      x: z.number(),
+      y: z.number(),
+    })
+  ),
+});
