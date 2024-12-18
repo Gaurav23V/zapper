a2d21a591462934de39314f895058b0a10ae0566
MajorVick <126904019+MajorVick@users.noreply.github.com>
2024-12-18 10:57:00 +0000

    authentication tests succeeding

--- mono/apps/http/src/routes/v1/index.ts
+++ mono/apps/http/src/routes/v1/index.ts
@@ -2,40 +2,82 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
-import { SignupSchema } from "../../types";
+import { SignupSchema, SigninSchema } from "../../types";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../../config";
+import { hash, compare } from "../../scrypt";
 import client from "@repo/db/client";
 
 export const router = Router();
 
 router.post("/signup", async (req, res) => {
+  console.log("inside signup")
   // check the user
-  const parsedData = SignupSchema.safeParse(req.body);
+  const parsedData = SignupSchema.safeParse(req.body)
   if (!parsedData.success) {
-    res.status(400).json({ error: parsedData.error });
-    return;
+      console.log("parsed data incorrect")
+      res.status(400).json({message: "Validation failed"})
+      return
+  }
+
+  const hashedPassword = await hash(parsedData.data.password)
+
+  try {
+       const user = await client.user.create({
+          data: {
+              username: parsedData.data.username,
+              password: hashedPassword,
+              role: parsedData.data.type === "admin" ? "Admin" : "User",
+          }
+      })
+      res.json({
+          userId: user.id
+      })
+  } catch(e) {
+      console.log("erroer thrown")
+      console.log(e)
+      res.status(400).json({message: "User already exists"})
+  }
+})
+
+router.post("/signin", async (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body)
+  if (!parsedData.success) {
+      res.status(403).json({message: "Validation failed"})
+      return
   }
 
   try {
-    // create the user
-    const user = await client.user.create({
-      data: {
-        username: parsedData.data.username,
-        password: parsedData.data.password,
-        role: parsedData.data.type === "admin" ? "Admin" : "User",
-      },
-    });
-    res.json({
-      userId: user.id,
-    });
-  } catch (e) {
-    res.status(400).json({ error: "User already exists" });
-    return;
-  }
-});
+      const user = await client.user.findUnique({
+          where: {
+              username: parsedData.data.username
+          }
+      })
 
-router.post("/signin", (req, res) => {
-  res.json({ message: "Signin!" });
-});
+      if (!user) {
+          res.status(403).json({message: "User not found"})
+          return
+      }
+      const isValid = await compare(parsedData.data.password, user.password)
+
+      if (!isValid) {
+          res.status(403).json({message: "Invalid password"})
+          return
+      }
+
+      const token = jwt.sign({
+          userId: user.id,
+          role: user.role
+      }, JWT_PASSWORD);
+
+      res.json({
+          token
+      })
+  } catch(e) {
+      res.status(400).json({message: "Internal server error"})
+  }
+})
+
 
 router.get("/avatars", (req, res) => {
   res.json({ message: "Avatars!" });
